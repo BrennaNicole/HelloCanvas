7895e4c28485839fd42fb59c74363c4275e808fa
BrennaNicole <BrieAnna.Allen@loop.colum.edu>
2018-09-30 22:25:16 -0500

    Add files via upload

---
+++ HelloCanvas/HelloCanvas.js
@@ -0,0 +1,17 @@
+﻿function main()
+{
+    var canvas = document.getElementById('webgl');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element');
+        return;
+    }
+
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
